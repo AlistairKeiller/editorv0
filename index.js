639e4692a2c9cc9e13f912705b61eb6185dfd9fa
AliCaliK1 <43255248+AliCaliK1@users.noreply.github.com>
2021-09-15 12:50:37 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,35 +1,25 @@
-<style>#editor {
-  position: absolute;
-  top: 0;
-  right: 0;
-  bottom: 0;
-  left: 0;
-}</style>
+var ace, WebSocketServer = require('ws').Server;
+require('fs').readFile('ace.html', function (err, data) {
+  ace = data;
+});
 
-<div id="editor">class Main {
-  public static void main(String[] args) {
-    System.out.println("Hello world!");
-  }
-}</div>
+require('http').createServer(function (req, res) {
+  res.end(ace);
+}).listen(8080);
 
-<script src="http://ajaxorg.github.io/ace-builds/src-min/ace.js"></script>
-<script src="http://ajaxorg.github.io/ace-builds/src-min/ext-language_tools.js"></script>
+server = new WebSocketServer({
+  port: 8081
+});
+sockets = [];
+server.on('connection', function(socket) {
+  sockets.push(socket);
 
-<script>require("ace/ext/language_tools");
-	editor = ace.edit("editor");
-  editor.setTheme("ace/theme/dracula");
-  editor.session.setMode("ace/mode/java");
-  editor.setOptions({
-    enableLiveAutocompletion: true
+  socket.on('message', function(msg) {
+    console.log(msg)
+    sockets.forEach(s => s.send(msg));
   });
 
-  const ws = new WebSocket('ws://http://ec2-54-176-173-178.us-west-1.compute.amazonaws.com:8081');
-  ws.onopen = function() {
-    editor.setValue('');
-    editor.getSession().on('change', function() {
-      socket.send(editor.getSession.getValue());
-    });
-  };
-  ws.onmessage = function(msg) {
-    editor.setValue(msg);
-  };</script>
+  socket.on('close', function() {
+    sockets = sockets.filter(s => s !== socket);
+  });
+});
