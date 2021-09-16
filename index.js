6024a44caa4f7820f04aa56fd9cbc76dc4e532b6
AliCaliK1 <43255248+AliCaliK1@users.noreply.github.com>
2021-09-15 22:43:56 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,50 @@
-var ace, WebSocketServer = require('ws').Server;
-require('fs').readFile('ace.html', function (err, data) {
-  ace = data;
-});
+const WebSocketServer = require('ws').Server, ace = `<style>#editor {
+  position: absolute;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  left: 0;
+}</style>
+
+<div id="editor"></div>
+
+<script src="http://ajaxorg.github.io/ace-builds/src-min/ace.js"></script>
+<script src="http://ajaxorg.github.io/ace-builds/src-min/ext-language_tools.js"></script>
+<script>require("ace/ext/language_tools");
+  editor = ace.edit("editor");
+  editor.setTheme("ace/theme/dracula");
+  editor.session.setMode("ace/mode/java");
+  editor.setOptions({
+    enableLiveAutocompletion: true
+  });
+
+  const ws = new WebSocket('ws://ec2-52-53-217-34.us-west-1.compute.amazonaws.com:8081');
+  eventsOn = true;
+
+  ws.onopen = function() {
+    editor.getSession().on('change', function(delta) {
+      if (eventsOn)
+        ws.send(JSON.stringify(delta));
+    });
+    editor.session.selection.on('changeCursor', function(e) {
+      console.log("test");
+    });
+  };
+
+  ws.onmessage = function(msg) {
+    msg = JSON.parse(msg.data);
+    eventsOn = false;
+    if(msg.action == "remove")
+      editor.session.remove({start: msg.start, end: msg.end});
+    else if(msg.action == "insert")
+      editor.session.insert(msg.start, msg.lines.join("\n"));
+    eventsOn = true;
+  };
+</script>`
+// var ace, WebSocketServer = require('ws').Server;
+// require('fs').readFile('ace.html', function (err, data) {
+//   ace = data;
+// });
 
 require('http').createServer(function (req, res) {
   res.end(ace);
