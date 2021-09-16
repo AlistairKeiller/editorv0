55f4b77c5d646cea6c78b2309b73cc5add1ad849
AliCaliK1 <43255248+AliCaliK1@users.noreply.github.com>
2021-09-16 16:51:50 -0700

    Update index.js

--- index.js
+++ index.js
@@ -52,7 +52,7 @@ const ace = `<script src="http://ajaxorg.github.io/ace-builds/src-min/ace.js"></
 wss = new (require('ws').Server)({server: server});
 
 wss.on('connection', function(ws, request) {
-  console.log(request['url'] instanceof String);
+  console.log(typeof request['url']);
 //   socket.on('message', function(msg) {
 // //     sockets.filter(s => s !== socket).forEach(s => s.send(msg.toString()));
 //   });
