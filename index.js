88e2dd9413503cda4f12b7c6aafc28e3ab13482d
AliCaliK1 <43255248+AliCaliK1@users.noreply.github.com>
2021-09-16 20:30:10 -0700

    Update index.js

--- index.js
+++ index.js
@@ -50,7 +50,7 @@ wss.on('connection', function(ws, request) {
 //   groups[group].forEach(member => workingWith[member].push(ws));
   groups[group].push(ws);
   
-  console.log(Object.keys(groups));
+  console.log(groups);
 
   ws.on('message', function(msg) {
     workingWith[ws].forEach(member => member.send(msg.toString()));
